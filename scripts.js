d5d928814070af2e85b0bc9f6cc82c9f387bf5e4
rokasandreikenas <shyptt@gmail.com>
2024-07-11 11:51:12 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-links a").forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+    });
+  });
+  // burger is workin
+});
